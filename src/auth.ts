b09593997c4d68e9bd47268cebedde72bb66ec90
yaowenchi <yaowenchi520@gmail.com>
2026-06-11 13:05:53 +0800

    fix

--- src/auth.ts
+++ src/auth.ts
@@ -24,5 +24,12 @@ export const auth = betterAuth({
         "http://localhost:5173", // 👈 允許本地端 Vite 預設網址
         "http://localhost:5174", // 👈 (選填) 有時 Vite 會跑到 5174，順便加上策安全
         "https://breakfast-order-system-1.onrender.com" // 👈 允許雲端 Render 網址
-    ] 
+    ] ,
+    //  強制允許跨網域 (Cross-Site) 寫入 Cookie
+    advanced: {
+        defaultCookieAttributes: {
+            sameSite: "none", // 允許跨網域傳輸
+            secure: true      // 綁定只能在 HTTPS 下運作
+        }
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -168,8 +168,7 @@ const app = new Elysia()
   .use(
     cors({
       origin: [
-        'http://localhost:5173', 
-        'http://localhost:5174', 
+        'http://localhost:5173',  
         'https://breakfast-order-system-1.onrender.com'
       ],
       credentials: true,
